ff55bccaa6bae27e820aac7b055a39e9513fc608
theo <thjmaurino@gmail.com>
2025-08-08 00:39:55 -0500

    might be closest to good animation i've gotten (optimism is a funny thing)

--- src/components/HomePageContent.jsx
+++ src/components/HomePageContent.jsx
@@ -4,86 +4,42 @@ import React, { useEffect, useState } from 'react'
 import HomeHeader from "@/components/HomeHeader";
 import StaticSketchedButton from "@/components/StaticSketchedButton";
 import { AnimatePresence, motion } from "framer-motion";
-import { useAnimationC } from '@/app/context/AnimationContext';
 
 const HomePageContent = () => {
 
 //   const {homePageVisited, setHomePageVisited} = useAnimationC();
-    const {homePageVisited, setHomePageVisited} = useAnimationC();
-    const [hasMounted, setHasMounted ] = useState(false);
+    // const {homePageVisited, setHomePageVisited} = useAnimationC();
+    const [shouldAnimate, setShouldAnimate] = useState(false);
+    const [animationComplete, setAnimationComplete] = useState(false);
 
     useEffect(() => {
-      setHasMounted(true);
-    }, [])
+      const hasAnimated = sessionStorage.getItem('hasAnimated');
 
-
-    // useEffect(() => {
-
-    //   // COULD STORE A TIMESTAMP IN SESSION STORAGE SO THAT AFTER X MINUTES WE MAKE THE ANIMATION PLAY AGAIN.
-
-    //   const hasVisited = sessionStorage.getItem("hasSeenIntro") === "true";
-    
-    //   if (!hasVisited) {
-    //       sessionStorage.setItem("hasSeenIntro", "true");
-    //       const timer = setTimeout(() => {
-    //           setHomePageVisited(true);
-    //       }, 1500);
-      
-    //       return () => clearTimeout(timer);
-
-    //   } else {
-    //     setHomePageVisited(true);
-    //   }
-
-    // }, []);   
-
-    // if (homePageVisited === null) {
-    //   return null; // could return a loader here
-    // }
-
-    useEffect(() => {
-
-      if (hasMounted && !homePageVisited) {
-        const timer = setTimeout(() => {
-            setHomePageVisited(true);
-        }, 1500);
-
-        return () => clearTimeout(timer);
+      if (!hasAnimated) {
+        sessionStorage.setItem("hasAnimated", true);
+        setShouldAnimate(true);
+      } else {
+        setAnimationComplete(true);
       }
 
-    }, [hasMounted, homePageVisited, setHomePageVisited])
+    }, [])
 
-    if (!hasMounted) {
-      // Render a server-safe version that matches SSR output
-      return (
-        // <div className="home-content-full w-full h-screen flex flex-col items-center justify-center">
-        //   <div className="z-10">
-        //     <HomeHeader />
-        //   </div>
-        // </div>
-
-        null
-      );
-    }
 
 
 
   return (
     <motion.div className="home-content-full w-full h-screen flex flex-col items-center justify-center"  
-        layout
-        transition={{duration: 0.8, ease: "easeInOut"}}
+        transition={{duration: 0.8, ease: "easeInOut"}} // maybe delete
       >
 
         {/* ANIMATE HEADER */}
         
         <motion.div
-          layout
-          initial={homePageVisited ? false : {scale : 0.8}}
-          animate={{
-            scale : homePageVisited ? 1 : 0.8,
-          }}
-          transition={{duration : 0.8, ease : "easeInOut"}}
+          initial={shouldAnimate ? {scale : 0.8} : false}
+          animate={shouldAnimate ? {scale : 1} : false}
+          transition={{duration : 0.8, ease : "easeInOut", delay : (shouldAnimate ? 1.5 : 0)}}
           className="z-10"
+          onAnimationComplete={() => setAnimationComplete(true)}
         >
           <HomeHeader />
         </motion.div>
@@ -91,14 +47,13 @@ const HomePageContent = () => {
         {/* Animate presence allows us to animate the introduction of new DOM trees */}
         <AnimatePresence>
 
-          {homePageVisited && (
+          {animationComplete && (
             <motion.section
               key="forward-buttons-section"
-              layout
-              initial={homePageVisited ? false : {opacity : 0, y : 20}}
+              initial={{opacity : 0, y : 20}}
               animate={{opacity : 1, y : 0}}
               exit={{opacity : 0, y: -20}}
-              transition={{duration : 0.6, delay : 1.2}}
+              transition={{duration : 0.6, delay : (shouldAnimate ? 2.4 : 0)}}
               className="flex flex-col gap-12 md:gap-10 mt-5 h-full"
             >
 
